67020dcd0ee7ccacdf55461bbd0e0912908d5d46
Fábio Moura de Oliveira <fbsharp@hotmail.com>
2017-04-26 15:26:18 -0300

    Add project files.

---
+++ MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MonkeyHubApp.ViewModels
+{
+    public class MainViewModel: BaseViewModel
+    {
+        private string _descricao;
+
+        public string Descricao
+        {
+            get { return _descricao; }
+            set {
+                _descricao = value;
+                SetProperty(ref _descricao, value);
+            }
+        }
+
+        private int _idade;
+
+        public int Idade
+        {
+            get { return _idade; }
+            set { _idade = value; SetProperty(ref _idade, value); }
+        }
+
+
+        public MainViewModel()
+        {
+            Descricao = "Olá, mundo, eu estou aqui";
+
+            Task.Delay(1000).ContinueWith( t =>
+            {
+                Descricao = "O texto alterou.";
+
+                for (int i = 0; i <= 10; i++)
+                {
+                    Task.Delay(1000);
+                    Descricao = $"Meu texto mudou {i}";
+                }
+                
+            });
+        }
+
+        
+    }
+}
